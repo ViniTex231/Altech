dfbe8adb31c46c2f7314ddd4ccc3921ced00351a
ViniTex231 <viniteixeira2828@gmail.com>
2023-07-14 10:58:30 -0300

    update js

--- js/script.js
+++ js/script.js
@@ -4,14 +4,15 @@ console.log(navbar)
 
 
 document.addEventListener('click', (e)=>{
-    const listaa =[]
+    const listaa = [document.querySelector('.active')]
     const liClick = e.target
     if(liClick.classList.contains('lis')){
-        listaa[0].classList.remove('active')
-        listaa.pop()
-        listaa.push(liClick)
-        listaa[0].classList.add('active')
- 
+        if (listaa.length > 0){
+            listaa[0].classList.remove('active')
+            listaa.pop()
+            listaa.push(liClick)
+            listaa[0].classList.add('active')
+        }
         } 
     }
   
